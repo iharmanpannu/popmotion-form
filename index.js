ee13e89881751538e7cad6a4383c6a4a122df272
iharmanpannu <iharmanpannu@gmail.com>
2017-12-23 22:15:52 -0800

    added new files

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const container = popmotion.styler(document.querySelector(".container"));
 const formElements = document.querySelector(".inner");
 const msgPop = popmotion.styler(document.querySelector(".msg"));
 
-popmotion.tween({
+const myAnim = popmotion.tween({
   from: {
     scale: 0.7,
     opacity: 0,
@@ -14,14 +14,32 @@ popmotion.tween({
     y: 0
   },
   duration: 1000
-}).start(container.set);
+});
 
-const stylers = Array
-  .from(formElements.children)
-  .map(popmotion.styler);
+const myAnim2 = popmotion.keyframes({
+  values: [
+    { y: -50, opacity: 0 },
+    { y: -20, opacity: 1 },
+    { y: -20, opacity: 1 },
+    { y: 0, opacity: 0 }
+  ],
+  times: [0, 0.2, 0.8, 1],
+  duration: 3000
+});
 
-const animations = Array(stylers.length).fill(popmotion.spring({from: 100, to: 0}));
+myAnim.start({
+  update: container.set,
+  complete: () => {
+    myAnim2.start(msgPop.set);
+  }
+});
+
+const stylers = Array.from(formElements.children).map(popmotion.styler);
+
+const animations = Array(stylers.length).fill(
+  popmotion.spring({ from: 100, to: 0 })
+);
 
 popmotion
   .stagger(animations, 100)
-  .start(v => v.forEach((x, i) => stylers[i].set("x", x)));
+  .start(v => v.forEach((x, i) => stylers[i].set({ y: x })));
